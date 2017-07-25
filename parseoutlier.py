b6df1174e2ef765ef0ac141d91efe5bc29ef8772
Julian Finn <julian@phinn.de>
2017-07-25 17:53:00 +0200

    fixed something

--- parseoutlier.py
+++ parseoutlier.py
@@ -30,7 +30,7 @@ while True:
 	output.readTelegramMessages()
 	print scheduled_tasks
 	for task in scheduled_tasks: 
-		f = open('outlier.log', "a")
+		f = open('outlier.log', "ab")
 		f.write(str(datetime.now())+": "+task+" called\n")
 		schedule_lasttime[task] = time.time()
 		if task == 'restock':

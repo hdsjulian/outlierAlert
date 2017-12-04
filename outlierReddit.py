642352016fc1d04c14a16f0195922fb5daa4244c
Julian Finn <julian@phinn.de>
2017-12-04 22:13:42 +0100

    syntax

--- outlierReddit.py
+++ outlierReddit.py
@@ -8,7 +8,7 @@ lasttime = time.time()
 class outlierReddit(object):
 	def __init__(self, output):
 		self.output = output
-		self.reddit = praw.Reddit(settings.client_id, redirect_uri='http://localhost:8080', settings.client_secret, settings.password, settings.username, settings.user_agent)
+		self.reddit = praw.Reddit(client_id=config.client_id, redirect_uri='http://localhost:8080', client_secret=config.client_secret, password=config.password, username=config.username, user_agent=config.user_agent)
 		self.subreddit = self.reddit.subreddit("outliermarket")
 		self.time = output.getRedditTime()
 		print self.time
